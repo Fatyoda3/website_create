b746e66643415508c6b829538edcf0b534f90d9a
Fat yoda <Fatyoda3@users.noreply.github.com>
2023-12-06 20:43:47 +0530

    trying some js on the go

---
+++ script.js
@@ -0,0 +1,33 @@
+const body = document.body;
+
+let buttons = [];
+const div = document.createElement("div");
+
+for (let i = 0; i < 5; i++) {
+  buttons[i] = document.createElement("button");
+  buttons[i].innerText = "hello " + i;
+
+  if (i == 3) div.appendChild(document.createElement("br"));
+  
+  div.appendChild(buttons[i]);
+
+  buttons[i].style.background = "violet";
+}
+
+
+
+
+body.appendChild(div);
+
+div.style.width  = "250px";
+
+div.style.marginLeft = "250px";
+
+div.style.border = " 10px solid orange";
+
+
+
+
+
+
+body.style.backgroundColor = "#0e0e00";
