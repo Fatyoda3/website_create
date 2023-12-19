ef09c4509ada9d6605a97b426a9398287c7b411d
Fat yoda <Fatyoda3@users.noreply.github.com>
2023-12-19 19:46:18 +0530

    hello

--- script.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector("button"); // Access the first element in the HTML collection
-let input = document.querySelector("input"); // Use querySelector to select the first input element
-
-let bye = () => console.log("hello ");
-
-const calculate = () => {
-    console.log(input.value); 
-    input.value= eval(input.value);// Access the value property of the input
-}
