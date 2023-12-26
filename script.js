d618833ebf2e52cb008c44219d44e9aee046e13d
Fat yoda <Fatyoda3@users.noreply.github.com>
2023-12-26 21:07:39 +0530

    hello practiced some array methods such as map filter some every find findindex  the syntax is some_what simillar to the es6 arrow function  let variableName = array.methodName(tempVar = tempVar some condition ); this is it

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ x.forEach((element) => {
 });
 */
 
-let LinearSearch = (key, high,arr) => {
+let LinearSearch = (key, high, arr) => {
   for (let i = 0; i < high; i++) {
     if (arr[i] === key) {
       console.log("found ");
@@ -55,5 +55,52 @@ let LinearSearch = (key, high,arr) => {
   return -1;
 };
 
+/* let Person = {
+  name: "ROHAN",
+  age: 20,
+  IsEmployed: true,
+};
+ */
+/* console.table(Person);
 
-console.log(LinearSearch(78,high,x));
+let child = Object(Person);
+child.name = "Jr. ROHAN";
+child.age = 8;
+child.IsEmployed = false; */
+/* 
+console.table(child); shift + alt + a to comment a piece of code 
+ */
+
+/* let xDouble = x.map(element =>console.log(element));
+//(element) => element * 2
+//console.table(xDouble);
+
+let xFilter = x.filter(element => element<58)
+
+//console.table(xFilter);
+
+ */
+
+//some
+
+/* let isIntegerArray = x.some((e) => e < 0);
+console.log(isIntegerArray);
+
+returns a boolean if some specific condition is met 
+ */
+
+//every array method
+//every element of the array must meet a specific criteria
+
+/* let isPositiveOnly = x.every((e) => e > 0);
+
+console.log(isPositiveOnly);
+ */
+
+ /*let Found = x.find((e) => e === 75);
+
+console.log(Found); 5
+returns the value if found in the array
+
+also we have findindex
+ */
