9e2090b1d6de1362113c562cbd25c12c79cacc6b
Fat yoda <Fatyoda3@users.noreply.github.com>
2023-12-27 10:28:08 +0530

    hello i learned switch case in js @ they are a lot better than the c version i learned falsy values there are probably 7 null undefined false documnet.all(object) 0 and -0 bigInt emptyString "  '' etc. this is all .

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let x = [1, 2, 3, 4, 5, 78, 95, 1255, 78, 95, 125];
+/* let x = [1, 2, 3, 4, 5, 78, 95, 1255, 78, 95, 125]; */
 
-let low = 0,
+/* let low = 0,
   high = x.length - 1;
 
 let key = 1;
 let mid;
-
-let bSearch = (key, high, low) => {
+ */
+/* let bSearch = (key, high, low) => {
   while (low <= high) {
     mid = Math.floor((low + high) / 2);
 
@@ -24,8 +24,8 @@ let bSearch = (key, high, low) => {
     }
   }
   return -1;
-};
-let bSort = (high, arr) => {
+}; */
+/* let bSort = (high, arr) => {
   let temp;
 
   for (let i = 0; i < high; i++) {
@@ -37,7 +37,7 @@ let bSort = (high, arr) => {
       }
     }
   }
-};
+}; */
 
 /*bSort(high, x);
 x.forEach((element) => {
@@ -45,7 +45,7 @@ x.forEach((element) => {
 });
 */
 
-let LinearSearch = (key, high, arr) => {
+/* let LinearSearch = (key, high, arr) => {
   for (let i = 0; i < high; i++) {
     if (arr[i] === key) {
       console.log("found ");
@@ -54,7 +54,7 @@ let LinearSearch = (key, high, arr) => {
   }
   return -1;
 };
-
+ */
 /* let Person = {
   name: "ROHAN",
   age: 20,
@@ -97,10 +97,100 @@ returns a boolean if some specific condition is met
 console.log(isPositiveOnly);
  */
 
- /*let Found = x.find((e) => e === 75);
+/*let Found = x.find((e) => e === 75);
 
 console.log(Found); 5
 returns the value if found in the array
 
 also we have findindex
  */
+
+//Case switch in js
+/* 
+const day = "wed";
+let month = 3;
+switch (day) {
+  case "mon":
+    console.log(day);
+
+    break;
+  case "tue":
+    console.log(day);
+    break;
+  case "wed":
+    console.log(day);
+    if (month) {
+      console.log(month, "is my lucky month number ! ");
+    }
+    break;
+  case "thu":
+    console.log(day);
+    break;
+  case "fri":
+    console.log(day);
+    break;
+  case "sat":
+    console.log(day);
+    break;
+  case "sun":
+    console.log(day);
+    break;
+  default:
+    console.log("not a right day ");
+    break;
+} */
+
+/*    false.if in a if statement this arguments are passed they take boolean 
+value of false
+    0 (zero)
+
+    '' or “” (empty string)
+
+    null
+
+    undefined
+
+    NaN
+
+    big INT 0n
+
+    document.all a inbuilt object that has a false value
+
+
+
+    code example
+
+    if (false) {
+  // Not reachable
+}
+
+if (null) {
+  // Not reachable
+}
+
+if (undefined) {
+  // Not reachable
+}
+
+if (0) {
+  // Not reachable
+}
+
+if (-0) {
+  // Not reachable
+}
+
+if (0n) {
+  // Not reachable
+}
+
+if (NaN) {
+  // Not reachable
+}
+
+if ("") {
+  // Not reachable
+}
+
+    */
+
