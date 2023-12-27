6d709f7c3b7c2944103706d4740a9f0cb9be27ac
Fat yoda <Fatyoda3@users.noreply.github.com>
2023-12-27 14:15:35 +0530

    improved some knowledge of fn and es6 arrow fn

--- advance.js
+++ advance.js
@@ -27,6 +27,49 @@ function hello() {
   console.log("object -- expression can't be called before declaration 11 ");
 };
 hi(); */
+/*let add = (a, b) => {
+  return a + b;
+};
 
+let sub = (a, b) => {
+  return a - b;
+};
 
+let mul = (a, b) => {
+  return a * b;
+};
 
+let div = (a, b) => {
+  return a / b;
+};
+
+let cal4 = function (num0, num1, OperationToPerform) {
+  return OperationToPerform(num0, num1);
+};
+
+let result = cal4(4, 4, add);
+console.log(result);
+*/
+// no need () if single argument will be passed to the arrow fn
+
+/* let cookin = bool => {
+  if (bool) console.log("we cookin hard bruh true  ");
+  else{
+    console.log("we cookin anyway false");
+  }
+};
+cookin(true);
+
+cookin(false);
+ */
+
+// this can be used do write large number 999_999_999_999_999
+
+let cooking = [10, 9, 8, 7, 8_9];
+
+console.table(cooking);
+
+if(cooking)
+{
+    console.log(true);
+}
