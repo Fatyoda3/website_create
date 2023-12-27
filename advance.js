e7b5a00e31e5b2fe3339dfdf5ecd9e3a080dcf58
Fat yoda <Fatyoda3@users.noreply.github.com>
2023-12-27 13:09:59 +0530

    hello i learned function expression and function delcaration it's simple

--- advance.js
+++ advance.js
@@ -10,3 +10,23 @@ so use strict mode always to help catch bugs
 let private = "12465";
 console.log(private);
  */
+
+//function expression vs declaration
+
+/* hello();
+function hello() {
+  console.log("object -- declaration can be called before declaration 00 ");
+}
+ */
+/* hi();  */
+//can't access lexical declaration 'hi' before initialization
+
+//error will be thrown at us
+
+/* let hi = function () {
+  console.log("object -- expression can't be called before declaration 11 ");
+};
+hi(); */
+
+
+
