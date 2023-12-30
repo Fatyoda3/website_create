43257adba315301b9ae638619fba54159d5fdd4e
Fat yoda <Fatyoda3@users.noreply.github.com>
2023-12-30 12:47:20 +0530

    hello learned a little bit about forms and eventListeners

--- DOM.js
+++ DOM.js
@@ -114,12 +114,136 @@ mango.classList.remove('tasty');
 
 //DOM element creation
 
-
 const body = document.body;
 
-let div = document.createElement('div');
-
+/* let div = document.createElement("div");
 
 body.appendChild(div);
-div.innerText = 'hello world ';
-div.style.fontSize = '64px';
+div.innerText = "hello world ";
+div.style.fontSize = "64px";
+
+//to give id or class to an html element
+
+
+console.log("class is ", div.className); 
+div.classList.add("hell");
+div.setAttribute("id", "angel");
+
+/* console.log("id is ", div.id); */
+/* let angel = document.querySelector("#angel");
+
+angel.style.boxShadow = "3px 5px 3px orange";
+
+const hell = document.querySelector(".hell");
+
+hell.style.textShadow = "1px 3px 8px red";
+
+const img = document.createElement("img"); */
+
+/* img.src =
+  "https://images.pexels.com/photos/19515316/pexels-photo-19515316/free-photo-of-top-view-of-boats-in-the-harbor.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load";
+
+  
+img.alt = "image boats ";
+
+
+ img.setAttribute(
+  "src",
+  "https://images.pexels.com/photos/19515316/pexels-photo-19515316/free-photo-of-top-view-of-boats-in-the-harbor.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+);
+
+img.setAttribute('alt', 'boats ');
+
+div.append(img); 
+
+ let arr = [25,88]
+
+//for each method can call a function over the value of elements in an array
+
+let double = (e) => e*e;
+arr.forEach((e)=>console.log(double(e)))
+ */
+
+// appendChild insertAdjacentElement
+
+/* const div = document.createElement("div");
+
+div.setAttribute("class", "parent");
+
+body.appendChild(div);
+
+const firstPara = document.createElement("p");
+
+const secPara = document.createElement("p");
+
+firstPara.innerHTML = "hello world this is para one ";
+
+secPara.innerHTML = "hello world this is second";
+ */
+/* div.appendChild(firstPara);
+No control over what get inserted where a linear order is followed 
+div.appendChild(secPara); */
+
+//better way
+
+/* div.appendChild(firstPara);
+// four methods before(beg , after , after end before
+
+firstPara.insertAdjacentElement("beforebegin", secPara);
+
+const button = document.createElement("button");
+button.innerText = "CLICK me ";
+
+body.appendChild(button);
+let clicks = 0;
+
+//anonymous function are inside event listener
+
+//es6 way of writing function
+
+button.addEventListener("click", () => {
+  console.log("clicked me ");
+  clicks++;
+  if (clicks >= 10) {
+    console.clear();
+    clicks = 0;
+  }
+});
+
+//standard way of writing function in js 
+
+
+button.addEventListener("click", function () {
+  console.log("hello world");
+});
+ */
+
+const timeStamp = document.createElement("p");
+
+const form = document.querySelector("#form");
+
+const button = document.querySelector("#btn");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(e.timeStamp);
+  timeStamp.innerHTML = `<h1>the submission time is ${e.timeStamp} </h1>`;
+});
+
+body.appendChild(timeStamp);
+
+const input = document.querySelector("#input");
+
+const text = document.querySelector(".text");
+
+input.addEventListener("keypress", (e) => {
+  console.log(e);
+  text.textContent += e.key;
+});
+
+const heading = document.querySelector('#Goal');
+
+heading.addEventListener("mouseover",()=>{
+  heading.style.color = "red";
+
+})
